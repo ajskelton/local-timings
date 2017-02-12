ad45cb5332313cfe7423a213388644c9b6ff4526
Anthony Skelton <anthony@Anthonys-MBP.ad.hatv.com>
2017-02-12 07:39:58 -0800

    Added html and bootstrap elements to top of file, removed buttons function and moved links into the nav bar

--- parse_pdf.php
+++ parse_pdf.php
@@ -1,6 +1,41 @@
-<?php
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Movies! Affiliates Local Break Times</title>
+        <meta name="description" content="Parsing Movies! Affiliates Timings PDF into Local Breaks List">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <!-- Place favicon.ico in the root directory -->
+        <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/css/main.css">
+    </head>
+    <body>
+    <nav class="navbar navbar-inverse">
+    	<div class="container">
+	    		<div class="navbar-header">
+	    			<span class="navbar-brand">KCRA | Movies! Affiliate Local Timings</span>
+	    		</div> <!-- .navbar-header -->
+	    		<div class="navbar-collapse">
+	    			<ul class="nav navbar-nav navbar-right">
+	    				<li>
+	    					<a href="http://10.151.4.115/movie-timings/index.php">Home</a>
+	    				</li>
+	    				<li>
+							<a href="javascript:window.print()">Print Page</a>
+						</li>
+	    			</ul>
+	    		</div> <!-- .navbar-collapse -->
+    		</div> <!-- .container -->
+    	</nav>
+    	<div class="container">
+
+<?php
 include 'src/autoload.php';
+
+$uploaddir = '';
+$uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
 
 function parse_pdf( $file ) {
 	$parser = new \Smalot\PdfParser\Parser();
@@ -14,12 +49,16 @@ function parse_pdf( $file ) {
 }
 
 function print_times( $array ) {
-
+	echo '<h2>' . $_FILES['userfile']['name'] . '</h2>';
+	echo '<div class="wrap">';
 	echo '<table>';
+	echo '<thead>';
 	echo '<tr>';
 	echo '<th>Break Time</th>';
 	echo '<th>Break Length</th>';
-	echo '<tr>';
+	echo '</tr>';
+	echo '</thead>';
+	echo '<tbody>';
 	for($i = 0; $i < count($array[0]); $i++){
 		echo '<tr>';
 		echo '<td>';
@@ -33,13 +72,15 @@ function print_times( $array ) {
 		echo '</td>';
 		echo '<td>';
 		echo ($array[1][$i]);
-		echo '<td>';
-		echo '<tr>';
+		echo '</td>';
+		echo '</tr>';
 	}
+	echo '</tbody>';
 	echo '</table>';
+	echo '</div>';
 }
 
-$pdf_text = parse_pdf( $file );
+$pdf_text = parse_pdf( $uploadfile );
 print_times( $pdf_text );
 
 ?>
